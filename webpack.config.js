b9145db6494766fd6d4facd15a683f0d5e1aa96c
Gaurav Gautam <gaurav.gautam@livspace.com>
2020-01-23 03:20:52 +0530

    Added webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,12 @@ module.exports = {
 	entry: "./src/index.js",
 	output: {
 		filename: "bundle.js",
-		path: path.resolve(__dirname, "public"),
+		path: path.resolve(__dirname, "dist"),
 		libraryTarget: "umd"
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, "public"),
+		compress: true,
+		port: 9000
 	}
 }
