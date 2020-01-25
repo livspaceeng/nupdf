47cb77583b3366aeac2f974f433929732fe83bbd
Gaurav Gautam <gaurav.gautam@livspace.com>
2020-01-25 10:26:28 +0530

    Added toolbar

--- src/components/textblock.js
+++ src/components/textblock.js
@@ -3,13 +3,16 @@ export default {
     content: {}
   },
   template: `
-    <div :style="{
-      border: 'solid 1px',
-      position: 'absolute',
-      left: content.attributes.x,
-      top: content.attributes.y,
-      width: content.attributes.options.width
-    }">
+    <div
+      class="textblock"
+      :style="{
+        border: 'solid 1px',
+        position: 'absolute',
+        left: content.attributes.x,
+        top: content.attributes.y,
+        width: content.attributes.options.width
+      }"
+    >
       {{ content.attributes.value }}
     </div>
   `
--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -1,3 +1,7 @@
 export default {
-  template: `<div class="toolbar">Tools</div>`
+  template: `
+    <div class="toolbar">
+      <span class="text">Tool</span>
+      <span class="fa fa-plus"></span>
+    </div>`
 }
