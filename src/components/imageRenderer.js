c5aab361d6688133d34f91ec7df6afce125908d1
Gaurav Gautam <gaurav.gautam@livspace.com>
2020-01-25 17:32:29 +0530

    Moved drag logic to page

--- src/components/imageRenderer.js
+++ src/components/imageRenderer.js
@@ -1,97 +1,4 @@
 export default {
-  props: {
-    content: {}
-  },
-  computed: {
-    imageurl() {
-      if (this.content.attributes.url) {
-        return this.content.attributes.url;
-      } else if (this.content.attributes.file) {
-        return URL.createObjectURL(this.content.attributes.file)
-      }
-    }
-  },
-  data: () => ({
-    mouseisdown: false,
-    down: { x: 0, y: 0 },
-    up: { x: 0, y: 0 },
-    start: { x: 0, y: 0 },
-    tophandle: {
-      down: { x: 0, y: 0 },
-      up: { x: 0, y: 0 },
-      start: { x: 0, y: 0 }
-    },
-    bottomhandle: {
-      down: { x: 0, y: 0 },
-      up: { x: 0, y: 0 },
-      start: { x: 0, y: 0 }
-    },
-    lefthandle: {
-      down: { x: 0, y: 0 },
-      up: { x: 0, y: 0 },
-      start: { x: 0, y: 0 }
-    },
-    righthandle: {
-      down: { x: 0, y: 0 },
-      up: { x: 0, y: 0 },
-      start: { x: 0, y: 0 }
-    },
-    hovered: false
-  }),
-  methods: {
-    mousedown(event) {
-      this.down = {x: event.x, y: event.y};
-      this.mouseisdown = true;
-      this.start.x = this.content.attributes.x;
-      this.start.y = this.content.attributes.y;
-    },
-    mouseup(event) {
-      this.mouseisdown = false;
-      this.$root.$emit('change');
-    },
-    move() {
-      if (this.mouseisdown) {
-        this.up = {x: event.x, y: event.y};
-        const dx = this.up.x - this.down.x;
-        const dy = this.up.y - this.down.y;
-        this.content.attributes.x = this.start.x + dx;
-        this.content.attributes.y = this.start.y + dy;
-        // console.log(dx, dy);
-      }
-    },
-    startTopResize(event) {},
-    endTopResize(event) {},
-    doTopResize(event) {},
-
-    startBottomResize(event) {},
-    endBottomResize(event) {},
-    doBottomResize(event) {},
-
-    startLeftResize(event) {},
-    endLeftResize(event) {},
-    doLeftResize(event) {},
-
-    startRightResize(event) {
-      this.righthandle.down = { x: event.x, y: event.y };
-      this.righthandle.start.x = this.content.attributes.options.width;
-      this.mouseisdown = true;
-    },
-    endRightResize(event) {
-      this.mouseisdown = false;
-      this.$root.$emit('change');
-    },
-    doRightResize(event) {
-      if (this.mouseisdown) {
-        this.righthandle.up = {x: event.x, y: event.y};
-        const dx = event.x - this.righthandle.down.x;
-        this.content.attributes.options.width = this.righthandle.start.x + dx;
-      }
-    },
-
-    setHovered(value) {
-      this.hovered = value;
-    }
-  },
   template: `
     <span class="hoverborder"
       :style="{
@@ -136,10 +43,102 @@ export default {
       <span class="handle handle-right"
         v-if="hovered"
         @mousedown="startRightResize"
-        @mouseup="endRightResize"
+        @mouseup="mouseup"
         @mousemove="doRightResize"
       >
       </span>
     </span>
-  `
+  `,
+  props: {
+    content: {}
+  },
+  computed: {
+    imageurl() {
+      if (this.content.attributes.url) {
+        return this.content.attributes.url;
+      } else if (this.content.attributes.file) {
+        return URL.createObjectURL(this.content.attributes.file)
+      }
+    }
+  },
+  data: () => ({
+    mouseisdown: false,
+    down: { x: 0, y: 0 },
+    up: { x: 0, y: 0 },
+    start: { x: 0, y: 0 },
+    tophandle: {
+      down: { x: 0, y: 0 },
+      up: { x: 0, y: 0 },
+      start: { x: 0, y: 0 }
+    },
+    bottomhandle: {
+      down: { x: 0, y: 0 },
+      up: { x: 0, y: 0 },
+      start: { x: 0, y: 0 }
+    },
+    lefthandle: {
+      down: { x: 0, y: 0 },
+      up: { x: 0, y: 0 },
+      start: { x: 0, y: 0 }
+    },
+    righthandle: {
+      down: { x: 0, y: 0 },
+      up: { x: 0, y: 0 },
+      start: { x: 0, y: 0 }
+    },
+    hovered: false
+  }),
+  methods: {
+    mousedown(event) {
+      this.$emit('pointerdown', { type: 'imagetap', x: event.x, y: event.y, docnode: this.content })
+      // this.down = {x: event.x, y: event.y};
+      // this.mouseisdown = true;
+      // this.start.x = this.content.attributes.x;
+      // this.start.y = this.content.attributes.y;
+    },
+    mouseup(event) {
+      // this.mouseisdown = false;
+      // this.$root.$emit('change');
+    },
+    move() {
+      // if (this.mouseisdown) {
+      //   this.up = {x: event.x, y: event.y};
+      //   const dx = this.up.x - this.down.x;
+      //   const dy = this.up.y - this.down.y;
+      //   this.content.attributes.x = this.start.x + dx;
+      //   this.content.attributes.y = this.start.y + dy;
+      //   // console.log(dx, dy);
+      // }
+    },
+
+    doRightResize(event) {
+      if (this.mouseisdown) {
+        this.righthandle.up = {x: event.x, y: event.y};
+        const dx = event.x - this.righthandle.down.x;
+        this.content.attributes.options.width = this.righthandle.start.x + dx;
+      }
+    },
+
+    startTopResize(event) {},
+    endTopResize(event) {},
+    doTopResize(event) {},
+
+    startBottomResize(event) {},
+    endBottomResize(event) {},
+    doBottomResize(event) {},
+
+    startLeftResize(event) {},
+    endLeftResize(event) {},
+    doLeftResize(event) {},
+
+    startRightResize(event) {
+      this.righthandle.down = { x: event.x, y: event.y };
+      this.righthandle.start.x = this.content.attributes.options.width;
+      this.mouseisdown = true;
+    },
+
+    setHovered(value) {
+      this.hovered = value;
+    }
+  }
 }
--- src/components/page.js
+++ src/components/page.js
@@ -3,6 +3,30 @@ import ImageRenderer from "./imageRenderer.js";
 import Rectangle from "./rectangle.js";
 
 export default {
+  template: `
+    <div class="page"
+      @mouseup="handleup"
+      @mousemove="handlemove"
+    >
+      <span v-for="element in page.children">
+        <TextBlock
+          v-if="element.type === 'text'"
+          :content="element"
+        />
+
+        <ImageRenderer
+          v-else-if="element.type === 'image'"
+          :content="element"
+          @pointerdown="handledown"
+        />
+
+        <Rectangle
+          v-else-if="element.type === 'rect'"
+          :content="element"
+        />
+      </span>
+    </div>
+  `,
   components: {
     TextBlock,
     ImageRenderer,
@@ -11,13 +35,39 @@ export default {
   props: {
     page: {}
   },
-  template: `
-    <div class="page">
-      <span v-for="element in page.children">
-        <TextBlock v-if="element.type === 'text'" :content="element" />
-        <ImageRenderer v-if="element.type === 'image'" :content="element"/>
-        <Rectangle v-if="element.type === 'rect'" :content="element"/>
-      </span>
-    </div>
-  `
+  data() {
+    return {
+      mouseIsDown: false,
+      mouseDownAt: { x: 0, y: 0 },
+      startEvent: '',
+      docNode: null,
+      nodeStartLocation: {x: 0, y: 0}
+    };
+  },
+  methods: {
+    handledown(event) {
+      this.mouseIsDown = true;
+      this.mouseDownAt = { x: event.x, y: event.y };
+      this.startEvent = event.type;
+      this.docnode = event.docnode;
+      this.nodeStartLocation = {
+        x: event.docnode.attributes.x,
+        y: event.docnode.attributes.y
+      };
+    },
+    handleup(event) {
+      this.mouseIsDown = false;
+      this.$root.$emit('change');
+    },
+    handlemove(event) {
+      if (this.mouseIsDown) {
+        if (this.startEvent === 'imagetap') {
+          const dx = event.x - this.mouseDownAt.x;
+          const dy = event.y - this.mouseDownAt.y;
+          this.docnode.attributes.x = this.nodeStartLocation.x + dx;
+          this.docnode.attributes.y = this.nodeStartLocation.y + dy;
+        }
+      }
+    }
+  }
 }
